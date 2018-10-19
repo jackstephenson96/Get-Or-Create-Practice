c26164e73d177ad4270f6bde6413f9bf3e7f2024
Jack <jackstephenson96@gmail.com>
2018-10-19 14:04:39 -0400

    lab finished

--- challenges.py
+++ challenges.py
@@ -19,7 +19,7 @@ app.debug = True
 app.config['SECRET_KEY'] = 'hardtoguessstringfromsi364thisisnotsupersecurebutitsok'
 
 ## Task 1: Create a database in postgresql in the code line below, and fill in your app's database URI.
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/discussion_songs_db"
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -35,71 +35,88 @@ manager.add_command('db', MigrateCommand) # Add migrate command to manager
 
 ##### Set up Models #####
 class Artist(db.Model):
-    __tablename__ = "artists"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    songs = db.relationship('Song',backref='Artist')
+	__tablename__ = "artists"
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64))
+	songs = db.relationship('Song',backref='Artist')
 
-    def __repr__(self):
-        return "{} (ID: {})".format(self.name,self.id)
+	def __repr__(self):
+		return "{} (ID: {})".format(self.name,self.id)
 
 class Song(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(64),unique=True) # Only unique title songs
-    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id")) # changed
-    genre = db.Column(db.String(64))
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(64),unique=True) # Only unique title songs
+	artist_id = db.Column(db.Integer, db.ForeignKey("artists.id")) # changed
+	genre = db.Column(db.String(64))
 
 
 ##### Set up Forms #####
 class SongForm(FlaskForm):
-    song = StringField("What is the title of your favorite song?", validators=[Required()])
-    artist = StringField("What is the name of the artist who performs it?",validators=[Required()])
-    genre = StringField("What is the genre of that song?", validators
-        =[Required()])
-    submit = SubmitField('Submit')
+	song = StringField("What is the title of your favorite song?", validators=[Required()])
+	artist = StringField("What is the name of the artist who performs it?",validators=[Required()])
+	genre = StringField("What is the genre of that song?", validators
+		=[Required()])
+	submit = SubmitField('Submit')
 
 ##### Helper functions
 ### For database additions / get_or_create functions
 
 def get_or_create_artist(artist_name):
-    # Query the artist table and filter using artist_name
-    # If artist exists, return the artist object
-    # Else add a new artist to the artist table
-    pass
+	a = db.session.query(Artist).filter_by(name=artist_name).first()
+	if a:
+		return a
+	else:
+		artist = Artist(name=artist_name)
+		db.session.add(artist)
+		db.session.commit()
+		return artist
 
 def get_or_create_song(song_title, song_artist, song_genre):
-    # Query the song table using song_title
-    # If song exists, return the song object
-    # Else add a new song to the song table.
-    # NOTE : You will need artist id because that is the foreign key in the song table.
-    # So if you are adding a new song, you will have to make a call to get_or_create_artist function using song_artist
-    pass
+	s = db.session.query(Song).filter_by(title=song_title).first()
+	if s:
+		return s
+	else:
+		artist = get_or_create_artist(song_artist)
+		song = Song(title=song_title, genre=song_genre, artist_id=artist.id)
+		db.session.add(song)
+		db.session.commit()
+		return song
+	# If song exists, return the song object
+	# Else add a new song to the song table.
+	# NOTE : You will need artist id because that is the foreign key in the song table.
+	# So if you are adding a new song, you will have to make a call to get_or_create_artist function using song_artist
 
 ##### Set up Controllers (view functions) #####
 
 ## Error handling routes
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+	return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+	return render_template('500.html'), 500
 
 ## Main route
 # Task 1.3 : Get the form data and add it to the database.
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    songs = Song.query.all()
-    num_songs = len(songs)
-    form = SongForm()
-    if form.validate_on_submit():
-        pass
-        ## Get the data from the form
-        ## Query the Song table using the song name to check if song exists.
-        #####   If song exists, reload the form and flash("You've already saved a song with that title!")
-        #####   Else use get_or_create_song function to add the song to the database, and after adding redirect to the /all_songs route.
-    return render_template('index.html', form=form,num_songs=num_songs)
+	songs = Song.query.all()
+	num_songs = len(songs)
+	form = SongForm()
+	if form.validate_on_submit():
+		if db.session.query(Song).filter_by(title=form.song.data).first():
+			flash("You've already saved a song with that title!")
+		# print(song.title)
+		# print(song.artist)
+		# print(song.genre)
+		else:
+			song = get_or_create_song(form.song.data, form.artist.data, form.genre.data)
+			return redirect(url_for('see_all'))
+		## Query the Song table using the song name to check if song exists.
+		#####   If song exists, reload the form and flash("You've already saved a song with that title!")
+		#####   Else use get_or_create_song function to add the song to the database, and after adding redirect to the /all_songs route.
+	return render_template('index.html', form=form,num_songs=num_songs)
 
 
 # Task 1.4: Update the view function.
@@ -114,13 +131,15 @@ def index():
 ############################################
 @app.route('/all_songs')
 def see_all():
-    return 'Update this view function to view all songs!'
+	return render_template('all_songs.html', all_songs={(song.title, str(Artist.query.filter_by(id=song.artist_id).first()).split("(")[0], song.genre) for song in Song.query.all()})
 
 @app.route('/all_artists')
 def see_all_artists():
-    return "Update this view function to view all artists!"
+	artists = Artist.query.all()
+	names = [a.name, len(Song.query.filter_by(artist_id=a.id).all() for a in artists)]
+	return render_template('all_artists', artist_names=names)
 
 if __name__ == '__main__':
-    db.create_all()
-    manager.run() # NEW: run with this: python main_app.py runserver
-    # Also provides more tools for debugging
+	db.create_all()
+	manager.run() # NEW: run with this: python main_app.py runserver
+	# Also provides more tools for debugging
